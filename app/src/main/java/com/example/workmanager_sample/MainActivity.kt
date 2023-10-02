0a5de56b536e5686ce2552726db58a1b04a5ce83
Arjun <71964107+arjun-sasidharan@users.noreply.github.com>
2023-10-02 20:47:42 +0530

    One time work request

--- app/src/main/java/com/example/workmanager_sample/MainActivity.kt
+++ app/src/main/java/com/example/workmanager_sample/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.workmanager_sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            setOneTimeWorkRequest()
+        }
     }
+
+
+    private fun setOneTimeWorkRequest() {
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .build()
+        WorkManager.getInstance(applicationContext)
+            .enqueue(uploadRequest)
+    }
+
 }
---
+++ app/src/main/java/com/example/workmanager_sample/UploadWorker.kt
@@ -0,0 +1,22 @@
+package com.example.workmanager_sample
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+
+    override fun doWork(): Result {
+        // code to execute deferrable task
+        try {
+            for (i in 0..600) {
+                Log.i("MYTAG", "Uploading $i")
+            }
+            return Result.success()
+        } catch (e: Exception) {
+            return Result.failure()
+        }
+
+    }
+}
